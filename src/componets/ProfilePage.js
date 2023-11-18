017ccf9181c66dc137e69c1562e1df23f305a6c7
Edric Owusu <eao20701@uga.edu>
2023-11-18 15:26:28 -0500

    Added Button and Card component and laid out basics for Profile page

--- src/componets/ProfilePage.js
+++ src/componets/ProfilePage.js
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import Card from './Card';
+import Button from './Button';
+import '../css/ProfilePage.css';
 
 function ProfilePage () {
 
+    var test = true;
+
+    var finalProfilePage =  
+    
+    <Card className="profile"><h1>Profile</h1>
+    Name: TestName
+    <br></br>
+    Email: testemail@gmail.com
+    <br></br>
+    Date Joined: ??/??/????
+    <br></br>
+    Workouts Created: 2
+    <br></br>
+    <center><Button className="logout">Log Out</Button></center>
+    </Card>;
+
+    if (test == false) {
+        finalProfilePage = <Card className="profile"><h1>Profile</h1>
+        <div className="notlogged">You are not logged in. Click below to either login or signup.</div>
+        </Card>
+        
+    }
+
     return (
-       <div></div>
+      finalProfilePage
     );
 }
 

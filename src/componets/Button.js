582e9309ef85e46ff29e8cbd737c4f3371acb945
Edric Owusu <eao20701@uga.edu>
2023-11-18 16:19:22 -0500

    Added buttons to profile page and linked the signup and login buttons

--- src/componets/Button.js
+++ src/componets/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import '../css/Button.css';
 
 const Button = (props) => {
--- src/componets/ProfilePage.js
+++ src/componets/ProfilePage.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button';
 import '../css/ProfilePage.css';
+import { Link } from 'react-router-dom';
 
 function ProfilePage () {
 
-    var test = true;
+    var test = false;
 
     var finalProfilePage =  
     
@@ -24,6 +25,11 @@ function ProfilePage () {
     if (test == false) {
         finalProfilePage = <Card className="profile"><h1>Profile</h1>
         <div className="notlogged">You are not logged in. Click below to either login or signup.</div>
+        <br></br>
+        <center>
+        <Link to='/signup'><Button className="register">Sign Up</Button></Link>
+        <Link to='/login'><Button className="getlogged">Log In</Button></Link>
+        </center>
         </Card>
         
     }

25d8931450b1d6acfcc8abda3b8b4bf692c049b6
Edric Owusu <eao20701@uga.edu>
2023-11-19 17:35:41 -0500

    Created basic Excercise componet

--- src/componets/ExcerciseList.js
+++ src/componets/ExcerciseList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Header from './Header';
+import '../css/ExcerciseList.css'
+import Excercise from './Excercise';
 
-function ExcerciseList () {
+function ExcerciseList (props) {
     return (
-        <div><Header></Header></div>
+        <div>
+            <Header></Header>
+            <h1>Excercises</h1>
+            <Excercise></Excercise>
+            <Excercise></Excercise>
+            <Excercise></Excercise>
+        </div>
     )
 }
 

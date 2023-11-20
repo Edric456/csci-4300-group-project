f29fa517d3f72fd48d58234d369f59d6c787f0ff
Edric Owusu <eao20701@uga.edu>
2023-11-19 20:56:39 -0500

    See previous commit but for delete and edit buttons

--- src/componets/Excercise.js
+++ src/componets/Excercise.js
@@ -4,9 +4,7 @@ import Button from "./Button";
 
 function Excercise (props) {
 
-    
-    return (
-    <div className="full-excercise">
+    var finalExcercise = <div className="full-excercise">
     <div className="body-part"><h2>Body Part</h2>{props.body}</div>
     <div className="equipment"><h2>Equipment</h2>{props.equipment}</div>
     <div className="excercise"><h2>Excercise</h2>{props.excercise}</div>
@@ -14,6 +12,20 @@ function Excercise (props) {
     <div className="edit"><Button className="edit-button">Edit</Button></div>
     <div className="delete"><Button className="delete-button">Delete</Button></div>
     </div>
+
+    if (props.logged === false) {
+        finalExcercise = <div className="full-excercise">
+        <div className="body-part"><h2>Body Part</h2>{props.body}</div>
+        <div className="equipment"><h2>Equipment</h2>{props.equipment}</div>
+        <div className="excercise"><h2>Excercise</h2>{props.excercise}</div>
+        <div className="image"><h2>Image</h2><img src={props.image} alt={props.excercise}></img></div>
+        <div className="edit">Login or Register to Edit</div>
+        <div className="delete">Login or Register to Delete</div>
+        </div>
+    }
+    
+    return (
+        finalExcercise
     )
 }
 
--- src/componets/ExcerciseList.js
+++ src/componets/ExcerciseList.js
@@ -11,7 +11,7 @@ function ExcerciseList (props) {
         <h1>Excercises</h1>
         {props.excercises.map((excercise) => 
             (<div>
-            <Excercise body={excercise.bodyPart} equipment={excercise.equipment} excercise={excercise.title} image={excercise.image}></Excercise>
+            <Excercise body={excercise.bodyPart} equipment={excercise.equipment} excercise={excercise.title} image={excercise.image} logged={props.logged}></Excercise>
             </div>))}
         <Link to="../excercises/create-excercise"><Button className="add">Add New Excercise</Button></Link>
     </div>
@@ -22,7 +22,7 @@ function ExcerciseList (props) {
         <h1>Excercises</h1>
         {props.excercises.map((excercise) => 
             (<div>
-            <Excercise body={excercise.bodyPart} equipment={excercise.equipment} excercise={excercise.title} image={excercise.image}></Excercise>
+            <Excercise body={excercise.bodyPart} equipment={excercise.equipment} excercise={excercise.title} image={excercise.image} logged={props.logged}></Excercise>
             </div>))}
         <div className="not-logged">Login or register to modify list of excercises.</div>
         <div className="two-buttons"><Link to='/signup'><Button className="register">Sign Up</Button></Link>

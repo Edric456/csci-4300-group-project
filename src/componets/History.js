a529a5ca9bc5d6b8e1c5dd8074a3ffeea5971e95
Edric Owusu <eao20701@uga.edu>
2023-11-29 01:18:07 -0500

    Added view of workouts for unauthenticatd users.

--- src/componets/History.js
+++ src/componets/History.js
@@ -12,6 +12,13 @@ function History (props) {
         <br/>
         <center><Link to="../history/create-routine"><Button className="adds">Add Workout</Button></Link></center>
         </div>
+
+    if (props.logged === false) {
+        finalWorkouts = <div><Header></Header><div className="not-logged">Login or register to access workout history.</div>
+        <div className="two-buttons"><Link to='/signup'><Button className="register">Sign Up</Button></Link>
+        <Link to='/login'><Button className="getlogged">Log In</Button></Link></div>
+    </div>
+    }
     return (
         finalWorkouts
     )
